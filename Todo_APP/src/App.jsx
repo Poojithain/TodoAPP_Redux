0cef199802f9c95c4591b8fe798c0a766aaa00af
poojithain <poojithin1@gmail.com>
2024-04-04 09:38:49 +0530

    mo

--- Todo_APP/src/App.jsx
+++ Todo_APP/src/App.jsx
@@ -1,29 +1,36 @@
-import  { useState } from 'react'
-import {useDispatch} from 'react-redux'
-import { addTodo } from './Actions'
-import TodoList from './TodoList'
+import  { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from './Actions';
+import TodoList from './TodoList';
 
 export default function App() {
-  const dispatch= useDispatch()
-  const [todoTitle, setTodoTitle]=useState('')
+  const dispatch = useDispatch();
+  const [todoTitle, setTodoTitle] = useState('');
+
   const handleTodo = () => {
     if (todoTitle.trim()) {
-        const newTodo = {
-            title: todoTitle
-        };
-        dispatch(addTodo(newTodo));
-        setTodoTitle(""); 
+      const newTodo = {
+        title: todoTitle
+      };
+      dispatch(addTodo(newTodo));
+      setTodoTitle("");
     }
-};
+  };
+
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' ,marginBottom:700 ,marginLeft:550}}>
       <h3>This is Todo Application using Redux</h3>
-      <div>
-        <input type="text" value={todoTitle} onChange={(e)=>setTodoTitle(e.target.value)}/>
-        <button onClick={()=>handleTodo()}>Add</button>
-       
+      <div style={{ marginTop: '20px' }}>
+        <input 
+          type="text" 
+          placeholder='Add you todos here...'
+          value={todoTitle} 
+          onChange={(e) => setTodoTitle(e.target.value)} 
+          style={{ width: '300px', padding: '10px', borderRadius: '5px', border: '1px solid #ccc' }} 
+        />
+        <button style={{backgroundColor:'green', marginLeft:20}}onClick={handleTodo}>Add</button>
       </div>
-      <TodoList/>
+      <TodoList />
     </div>
-  )
+  );
 }
